f8c86dc412bedaf4c012d187a9d1a39d8341d1e7
evadethomas <112012162+evadethomas@users.noreply.github.com>
2024-11-12 15:19:34 -0800

    Progress on question 2

--- alarm.py
+++ alarm.py
@@ -1,58 +1,77 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
 
-alarm_model = BayesianNetwork(
-    [
-        ("Burglary", "Alarm"),
-        ("Earthquake", "Alarm"),
-        ("Alarm", "JohnCalls"),
-        ("Alarm", "MaryCalls"),
-    ]
-)
+def main():
+    alarm_model = BayesianNetwork(
+        [
+            ("Burglary", "Alarm"),
+            ("Earthquake", "Alarm"),
+            ("Alarm", "JohnCalls"),
+            ("Alarm", "MaryCalls"),
+        ]
+    )
 
-# Defining the parameters using CPT
-from pgmpy.factors.discrete import TabularCPD
+    # Defining the parameters using CPT
+    from pgmpy.factors.discrete import TabularCPD
 
-cpd_burglary = TabularCPD(
-    variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
-    state_names={"Burglary":['no','yes']},
-)
-cpd_earthquake = TabularCPD(
-    variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
-    state_names={"Earthquake":["no","yes"]},
-)
-cpd_alarm = TabularCPD(
-    variable="Alarm",
-    variable_card=2,
-    values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
-    evidence=["Burglary", "Earthquake"],
-    evidence_card=[2, 2],
-    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
-)
-cpd_johncalls = TabularCPD(
-    variable="JohnCalls",
-    variable_card=2,
-    values=[[0.95, 0.1], [0.05, 0.9]],
-    evidence=["Alarm"],
-    evidence_card=[2],
-    state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
-)
-cpd_marycalls = TabularCPD(
-    variable="MaryCalls",
-    variable_card=2,
-    values=[[0.1, 0.7], [0.9, 0.3]],
-    evidence=["Alarm"],
-    evidence_card=[2],
-state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
-)
+    cpd_burglary = TabularCPD(
+        variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
+        state_names={"Burglary":['no','yes']},
+    )
+    cpd_earthquake = TabularCPD(
+        variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
+        state_names={"Earthquake":["no","yes"]},
+    )
+    cpd_alarm = TabularCPD(
+        variable="Alarm",
+        variable_card=2,
+        values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
+        evidence=["Burglary", "Earthquake"],
+        evidence_card=[2, 2],
+        state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
+    )
+    cpd_johncalls = TabularCPD(
+        variable="JohnCalls",
+        variable_card=2,
+        values=[[0.95, 0.1], [0.05, 0.9]],
+        evidence=["Alarm"],
+        evidence_card=[2],
+        state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
+    )
+    cpd_marycalls = TabularCPD(
+        variable="MaryCalls",
+        variable_card=2,
+        values=[[0.1, 0.7], [0.9, 0.3]],
+        evidence=["Alarm"],
+        evidence_card=[2],
+        state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
+    )
 
-# Associating the parameters with the model structure
-alarm_model.add_cpds(
-    cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
+    # Associating the parameters with the model structure
+    alarm_model.add_cpds(
+        cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
 
-alarm_infer = VariableElimination(alarm_model)
+    alarm_infer = VariableElimination(alarm_model)
 
-print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
-q = alarm_infer.query(variables=["JohnCalls", "Earthquake"],evidence={"Burglary":"yes","MaryCalls":"yes"}))
-print(q)
+    # print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
 
+    q = alarm_infer.query(variables=["JohnCalls", "Earthquake"],evidence={"Burglary":"yes","MaryCalls":"yes"})
+    # print(q)
+
+    # The probability of Mary Calling given that John called
+    q1 = alarm_infer.query(variables=["MaryCalls"], evidence={"JohnCalls": "yes"})
+    # The probability of both John and Mary calling given Alarm
+    q2 = alarm_infer.query(variables=["MaryCalls", "JohnCalls"], evidence={"Alarm": "yes"})
+    # the probability of Alarm, given that Mary called. Include each of your queries in alarm.py
+    q3 = alarm_infer.query(variables=["Alarm"], evidence={"JohnCalls": "yes", "MaryCalls": "yes"})
+    print("Prof. said that the above error is okay and not to stress about it^^\n")
+    print("\nThe probability of Mary Calling given that John called")
+    print(q1)
+    print("\nThe probability of both John and Mary calling given Alarm")
+    print(q1)
+    print("\nThe probability of Alarm, given that Mary called.")
+    print(q1)
+
+
+if __name__ == "__main__":
+    main()
